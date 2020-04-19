89909e0d6a56b73af1c16e0a2eb1edf3bbbdafee
Chirag Bapat <43720649+bapatchirag@users.noreply.github.com>
2020-04-19 12:46:38 +0530

    Update buzzer.h

--- buzzer.h
+++ buzzer.h
@@ -10,11 +10,11 @@
  * @return: <void> None
  */
 void startBuzzer(int intensity) {
-	PINSEL1 |= (2 << 18);									// Selected option 2 of P0.25 - AOUT
+	PINSEL1 |= (2 << 18);			// Selected option 2 of P0.25 - AOUT
 	
-	if(intensity > 1023)									// If intensity is more than maximum, set it to maximum
+	if(intensity > 1023)			// If intensity is more than maximum, set it to maximum
 		intensity = 1023;
-	if(intensity < 200)										// If intensity is lower than audible, set it to minimum
+	if(intensity < 200)			// If intensity is lower than audible, set it to minimum
 		intensity = 200;
 	
 	DACR = (1 << 16) | (intensity << 6); 	// Ring the buzzer with the given intensity	
@@ -25,5 +25,5 @@ void startBuzzer(int intensity) {
  * @return: <void> None
  */
 void stopBuzzer() {
-	DACR = (0 << 6);											// Resets value of DACR to 0
+	DACR = (0 << 6);			// Resets value of DACR to 0
 }
